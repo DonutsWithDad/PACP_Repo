2885e06b400d324d43280d394074ddad4f5d9724
Colbin Hobbs <81337598+DonutsWithDad@users.noreply.github.com>
2023-02-16 10:38:35 -0600

    psuedocode implemented in assingment 2

--- Assignment_2/Assignment_2.c
+++ Assignment_2/Assignment_2.c
@@ -1,8 +1,8 @@
 // =================================== //
 // Matthew Moffitt                     //
-// Assignment 1                        //
+// Assignment 2                        //
 // Parallel And Concurrent Programming //
-// Due Feb. 7, before midnight         //
+// Due Feb. 21, before midnight         //
 // =================================== //
 
 #include <stdio.h>
@@ -20,13 +20,32 @@ void main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
 
+    // Outcome will be three different prints of the same row sum from all of the processes
+
     if (pid == 0)
     {
-
+        //generate some data (maybe 30 rows of data for row sum)
+        //send 10 rows to pid 1
+        //send another 10 rows to pid 2
+        // ** evaluate lat 10 rows here **//
+        // ** receive all data here from pid 1 and 2 ** // Causing barrier
+        // print ALL row sums from pid 0
     }
-    else /*** pid == 1 ***/
+    else if (pid == 1)
     {
-
-    } /****** End of else ******/
+        // recieve data here
+        // evaluate row sums
+        // ** send to other pids **//
+        // ** recieve from 0 and 2 **// Causing our barrier
+        // print all row sums
+    }
+    else /*** pid == 2 ***/
+    {
+        // recieve data here
+        // evaluate row sums
+        // ** send data to other pids **//
+        // ** recieve data from pid 1 and 0 **// causing our barrier
+        // print all row sums
+    }/****** End of else ******/
     MPI_Finalize();
 }
