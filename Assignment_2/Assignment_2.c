ebd7a5b12b1a325272aeaf9ff69f73e440f4b6e1
DonutsWithDad <colbin.hobbs@ttu.edu>
2023-02-19 13:02:54 -0600

    runs in 2n time

--- Assignment_2/Assignment_2.c
+++ Assignment_2/Assignment_2.c
@@ -12,40 +12,52 @@
 
 void main(int argc, char **argv)
 {
-    int i, j, pid, np, mtag, count;//, data[100][100], data_recv[100], row_sum[100];
+    int i, j, pid, np, mtag, count;
     double t0, t1;
     MPI_Status status;
     MPI_Request req_s, req_r, req_finished;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
     MPI_Comm_size(MPI_COMM_WORLD, &np);
+    int data[np];
 
     // Outcome will be three different prints of the same row sum from all of the processes
 
     if (pid == 0)
     {
-        //generate some data (maybe 30 rows of data for row sum)
-        //send 10 rows to pid 1
-        //send another 10 rows to pid 2
-        // ** evaluate lat 10 rows here **//
-        // ** receive all data here from pid 1 and 2 ** // Causing barrier
-        // print ALL row sums from pid 0
+        for (int i = 0; i < np; i++){
+            data[i] = pid;
+        }
+        mtag = 1;
+        for (int i = 0; i < np; i++){
+            data[pid] = data[pid] * pid;
+            if (i != pid){
+                MPI_Isend(data, 10, MPI_INT, i, mtag, MPI_COMM_WORLD, &req_s);
+            }
+        }
+        for (int i = 0; i < np; i++){
+            if (i != pid){
+                MPI_Recv(data, 10, MPI_INT, i, mtag, MPI_COMM_WORLD, &status);
+            }
+        }
+        for (int i = 0; i < np; i++){
+            printf("%d\n", data[i]);
+        }
     }
-    else if (pid == 1)
+    else /*** all other processes ***/
     {
-        // recieve data here
-        // evaluate row sums
-        // ** send to other pids **//
-        // ** recieve from 0 and 2 **// Causing our barrier
-        // print all row sums
-    }
-    else /*** pid == 2 ***/
-    {
-        // recieve data here
-        // evaluate row sums
-        // ** send data to other pids **//
-        // ** recieve data from pid 1 and 0 **// causing our barrier
-        // print all row sums
+        mtag = 1;
+        for (int i = 0; i < np; i++){
+            data[pid] = data[pid] * pid;
+            if (i != pid){
+                MPI_Isend(data, 10, MPI_INT, i, mtag, MPI_COMM_WORLD, &req_s);
+            }
+        }
+        for (int i = 0; i < np; i++){
+            if (i != pid){
+                MPI_Recv(data, 10, MPI_INT, i, mtag, MPI_COMM_WORLD, &status);
+            }
+        }
     }/****** End of else ******/
     MPI_Finalize();
 }
